d509146c32839bc87657e1881fa82583cfe6e968
griffith <xjulianbarragan@gmail.com>
2025-12-02 22:51:11 +0100

    completed day 2 part 1

---
+++ day2/p1.cpp
@@ -0,0 +1,59 @@
+#include <print>
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+struct Range {
+	unsigned long int start;
+	unsigned long int end;
+};
+
+std::vector<Range> read_ranges() {
+	std::vector<Range> ranges;
+
+	std::string line; // buffer for line
+	std::getline(std::cin, line); // read from sdtin to buffer
+
+	std::istringstream ss(line);
+	std::string token; // buffer for each comma separated token
+
+
+	while (std::getline(ss, token, ',')) {
+		int dash_idx = token.find('-');
+		ranges.push_back({
+			std::stoul(token.substr(0, dash_idx)),
+			std::stoul(token.substr(dash_idx + 1))
+		});
+	}
+
+	return ranges;
+}
+
+
+int main() {
+	std::vector<Range> ranges = read_ranges();
+	std::vector<unsigned long int> invalid_ids;
+	std::string str_i;
+	std::string fst_half;
+	std::string snd_half;
+	for (const auto& r : ranges) {
+		for (unsigned long int i = r.start; i <= r.end; i++) {
+			str_i = std::to_string(i);
+			if (str_i.size() % 2 != 0) { continue; } // can only check even length sequences for match
+			fst_half = str_i.substr(0, str_i.size() / 2);
+			snd_half = str_i.substr(str_i.size() / 2);
+			if (fst_half == snd_half) {
+				invalid_ids.push_back(i);
+				std::println("Invalid ID found: {}", i);
+			}
+		}
+	}
+	unsigned long long int id_sum = std::accumulate(
+		invalid_ids.begin(),
+		invalid_ids.end(),
+		0ULL
+	);
+	std::println("The answer is: {}", id_sum);
+}
